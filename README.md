0186b7a1b366dddd35b6c2098eb4da8f5112bcc5
twk <1024565378@qq.com>
2023-05-06 15:18:13 +0800

    !

--- README.md
+++ README.md
@@ -39,8 +39,8 @@
 >合并llama_7b_hf、chinese_llama_plus_lora_7b、chinese_alpaca_plus_lora_7b为chinese_alpaca_plus_7b_hf  
 >```
 >python merge_model.py --base_model /TRAINING_CACHE/llama_7b_hf --lora_model /TRAINING_CACHE/chinese_llama_plus_lora_7b,/TRAINING_CACHE/chinese_alpaca_plus_lora_7b --output_type huggingface --output_dir /TRAINING_CACHE/chinese_alpaca_plus_7b_hf  
+>如果内存不够可使用缓存，命令中加上缓存路径：--offload_dir cache
 >```
->如果内存不够可使用缓存，命令中加上缓存路径```--offload_dir cache```  
 ### *，直接下载chinese_alpaca_plus_7b_hf
 >huggingface地址：https://huggingface.co/shibing624/chinese-alpaca-plus-7b/tree/main  
 >```
@@ -49,9 +49,16 @@
 >git clone https://huggingface.co/shibing624/chinese-alpaca-plus-7b：克隆项目  
 >```
 ### 6，inference_hf.py
->启动模型  
+>启动模型(不具备记忆功能)  
+>交互式：  
 >```
 >python inference_hf.py --base_model /TRAINING_CACHE/chinese_alpaca_plus_7b_hf --with_prompt --interactive  
+>```
+>读取文件：  
+>```
+>python inference_hf.py --base_model /TRAINING_CACHE/chinese_alpaca_plus_7b_hf --with_prompt --data_file data.txt --predictions_file result.txt  
+>--data_file：待预测数据，按行读取  
+>--predictions_file：存放预测结果  
 >```
 ### 其他
 >github链接：https://github.com/TWK2022/ChineseAlpacaPlus7b  
--- inference_hf.py
+++ inference_hf.py
@@ -1,15 +1,17 @@
 import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer
-from peft import  PeftModel
+from peft import PeftModel
 import argparse
 import json, os
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--base_model', default=None, type=str, required=True)
-parser.add_argument('--lora_model', default=None, type=str,help="If None, perform inference on the base model")
-parser.add_argument('--tokenizer_path',default=None,type=str)
-parser.add_argument('--data_file',default=None, type=str,help="file that contains instructions (one instruction per line).")
-parser.add_argument('--with_prompt',action='store_true')
-parser.add_argument('--interactive',action='store_true')
+parser.add_argument('--lora_model', default=None, type=str, help="If None, perform inference on the base model")
+parser.add_argument('--tokenizer_path', default=None, type=str)
+parser.add_argument('--data_file', default=None, type=str,
+                    help="file that contains instructions (one instruction per line).")
+parser.add_argument('--with_prompt', action='store_true')
+parser.add_argument('--interactive', action='store_true')
 parser.add_argument('--predictions_file', default='./predictions.json', type=str)
 args = parser.parse_args()
 
@@ -21,12 +23,11 @@ generation_config = dict(
     num_beams=1,
     repetition_penalty=1.3,
     max_new_tokens=400
-    )
+)
 
-
- # The prompt template below is taken from llama.cpp
- # and is slightly different from the one used in training.
- # But we find it gives better results
+# The prompt template below is taken from llama.cpp
+# and is slightly different from the one used in training.
+# But we find it gives better results
 prompt_input = (
     "Below is an instruction that describes a task. "
     "Write a response that appropriately completes the request.\n\n"
@@ -34,6 +35,7 @@ prompt_input = (
 )
 
 sample_data = ["为什么要减少污染，保护环境？"]
+
 
 def generate_prompt(instruction, input=None):
     if input:
@@ -58,29 +60,29 @@ if __name__ == '__main__':
         load_in_8bit=False,
         torch_dtype=load_type,
         low_cpu_mem_usage=True,
-        )
+    )
 
     model_vocab_size = base_model.get_input_embeddings().weight.size(0)
     tokenzier_vocab_size = len(tokenizer)
     print(f"Vocab of the base model: {model_vocab_size}")
     print(f"Vocab of the tokenizer: {tokenzier_vocab_size}")
-    if model_vocab_size!=tokenzier_vocab_size:
+    if model_vocab_size != tokenzier_vocab_size:
         assert tokenzier_vocab_size > model_vocab_size
         print("Resize model embeddings to fit tokenizer")
         base_model.resize_token_embeddings(tokenzier_vocab_size)
     if args.lora_model is not None:
         print("loading peft model")
-        model = PeftModel.from_pretrained(base_model, args.lora_model,torch_dtype=load_type)
+        model = PeftModel.from_pretrained(base_model, args.lora_model, torch_dtype=load_type)
     else:
         model = base_model
 
-    if device==torch.device('cpu'):
+    if device == torch.device('cpu'):
         model.float()
     # test data
     if args.data_file is None:
         examples = sample_data
     else:
-        with open(args.data_file,'r') as f:
+        with open(args.data_file, 'r') as f:
             examples = [l.strip() for l in f.readlines()]
         print("first 10 examples:")
         for example in examples[:10]:
@@ -94,27 +96,27 @@ if __name__ == '__main__':
             print("Start inference with interactive mode.")
             while True:
                 raw_input_text = input("Input:")
-                if len(raw_input_text.strip())==0:
+                if len(raw_input_text.strip()) == 0:
                     break
                 if args.with_prompt:
                     input_text = generate_prompt(instruction=raw_input_text)
                 else:
                     input_text = raw_input_text
-                inputs = tokenizer(input_text,return_tensors="pt")  #add_special_tokens=False ?
+                inputs = tokenizer(input_text, return_tensors="pt")  # add_special_tokens=False ?
                 generation_output = model.generate(
-                    input_ids = inputs["input_ids"].to(device),
-                    attention_mask = inputs['attention_mask'].to(device),
+                    input_ids=inputs["input_ids"].to(device),
+                    attention_mask=inputs['attention_mask'].to(device),
                     eos_token_id=tokenizer.eos_token_id,
                     pad_token_id=tokenizer.pad_token_id,
                     **generation_config
                 )
                 s = generation_output[0]
-                output = tokenizer.decode(s,skip_special_tokens=True)
+                output = tokenizer.decode(s, skip_special_tokens=True)
                 if args.with_prompt:
                     response = output.split("### Response:")[1].strip()
                 else:
                     response = output
-                print("Response: ",response)
+                print("Response: ", response)
                 print("\n")
         else:
             print("Start inference.")
@@ -124,16 +126,16 @@ if __name__ == '__main__':
                     input_text = generate_prompt(instruction=example)
                 else:
                     input_text = example
-                inputs = tokenizer(input_text,return_tensors="pt")  #add_special_tokens=False ?
+                inputs = tokenizer(input_text, return_tensors="pt")  # add_special_tokens=False ?
                 generation_output = model.generate(
-                    input_ids = inputs["input_ids"].to(device),
-                    attention_mask = inputs['attention_mask'].to(device),
+                    input_ids=inputs["input_ids"].to(device),
+                    attention_mask=inputs['attention_mask'].to(device),
                     eos_token_id=tokenizer.eos_token_id,
                     pad_token_id=tokenizer.pad_token_id,
                     **generation_config
                 )
                 s = generation_output[0]
-                output = tokenizer.decode(s,skip_special_tokens=True)
+                output = tokenizer.decode(s, skip_special_tokens=True)
                 if args.with_prompt:
                     response = output.split("### Response:")[1].strip()
                 else:
@@ -142,11 +144,11 @@ if __name__ == '__main__':
                 print(f"Input: {example}\n")
                 print(f"Output: {response}\n")
 
-                results.append({"Input":input_text,"Output":response})
+                results.append({"Input": input_text, "Output": response})
 
             dirname = os.path.dirname(args.predictions_file)
-            os.makedirs(dirname,exist_ok=True)
-            with open(args.predictions_file,'w') as f:
-                json.dump(results,f,ensure_ascii=False,indent=2)
-            with open(dirname+'/generation_config.json','w') as f:
-                json.dump(generation_config,f,ensure_ascii=False,indent=2)
+            os.makedirs(dirname, exist_ok=True)
+            with open(args.predictions_file, 'w') as f:
+                json.dump(results, f, ensure_ascii=False, indent=2)
+            with open(dirname + '/generation_config.json', 'w') as f:
+                json.dump(generation_config, f, ensure_ascii=False, indent=2)
